944c737533965d7ed45e36779a0b11e36a81be5d
kennycallado <kennycallado@hotmail.com>
2024-04-24 16:17:43 +0200

    UPDATE

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,11 +1,13 @@
 #! /usr/bin/env bash
 
-mkdir -p "${HOME}/.local/bin"
-PATH="${PATH}:${HOME}/.local/bin"
+# mkdir -p "${HOME}/.local/bin"
+# PATH="${PATH}:${HOME}/.local/bin"
+
+echo "hola"
 
 # bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release)
 # LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh) --no-install-dependencies -y
 
 # ln -sf $PWD/.devcontainer/editor.lua $HOME/.config/lvim/config.lua && set +x
 
-exit 0
+# exit 0
