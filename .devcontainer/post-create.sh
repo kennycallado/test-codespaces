4a69ad8a2ea58158ddf87ef6c3e9f756257e9cad
kennycallado <kennycallado@hotmail.com>
2024-04-30 14:32:00 +0200

    FIX

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
-mkdir -p "${HOME}/.local/bin"
-PATH="${PATH}:${HOME}/.local/bin"
+mkdir -p "$HOME/.local/bin"
+PATH="$PATH:$HOME/.local/bin"
 
 bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release)
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh) --no-install-dependencies -y
